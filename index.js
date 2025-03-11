0d2eeb21d9aa054e3932704af67c460b55d566dc
kennywestt <dlrudrms0617@naver.com>
2025-03-11 23:26:15 +0900

    access-contorl-allow change

--- index.js
+++ index.js
@@ -35,6 +35,14 @@ console.log("env service: ",process.env.SERVICE_ADDRESS);
 
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // 모든 도메인 허용
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true"); // 인증 정보 허용
+    next();
+});
+
 
 
 app.use(bodyParser.json());
